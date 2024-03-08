79daa393f7a777c44fe98bf0f50f1728b61f1da6
Oksana Shukh <oksana@zephyros.design>
2024-03-07 20:46:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleClick = (value) => {
+    if (value === "=") {
+      setResult(eval(input).toString());
+    } else if (value === "C") {
+      setInput("");
+      setResult("");
+    } else {
+      setInput((prev) => prev + value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="flex-style">
+        <input type="text" value={input} readOnly />
+        <input type="text" value={result} readOnly />
+      </div>
+      <div className="grid-container">
+        <button className="grid-item" onClick={() => handleClick("1")}>
+          1
+        </button>
+        <button className="grid-item" onClick={() => handleClick("2")}>
+          2
+        </button>
+        <button className="grid-item" onClick={() => handleClick("3")}>
+          3
+        </button>
+
+        <button className="grid-item" onClick={() => handleClick("4")}>
+          4
+        </button>
+        <button className="grid-item" onClick={() => handleClick("5")}>
+          5
+        </button>
+        <button className="grid-item" onClick={() => handleClick("6")}>
+          6
+        </button>
+        <button className="grid-item" onClick={() => handleClick("7")}>
+          7
+        </button>
+        <button className="grid-item" onClick={() => handleClick("8")}>
+          8
+        </button>
+        <button className="grid-item" onClick={() => handleClick("9")}>
+          9
+        </button>
+        <button onClick={() => handleClick("*")}>*</button>
+        <button onClick={() => handleClick("/")}>/</button>
+        <button onClick={() => handleClick("+")}>+</button>
+        <button onClick={() => handleClick("-")}>-</button>
+        <button onClick={() => handleClick(".")}>.</button>
+
+        <button onClick={() => handleClick("0")}>0</button>
+      </div>
+      <div className="grid-side">
+        <button onClick={() => handleClick("=")}>=</button>
+        <button onClick={() => handleClick("C")}>C</button>
+      </div>
     </div>
   );
 }
